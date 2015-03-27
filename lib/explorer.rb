a8dcca2b19b211269996b1a4d91cb2bb3a5d23da
Amireza <arfo.amireza@gmail.com>
2015-03-28 02:52:14 +0800

    Adding explore method to the explorer class
    This method responsibility is to look for surrounding cells and returns
    number of alive cells. for now it faile in case of of first indix of grid since
    iteration hardcoded and is not dynamic

--- lib/explorer.rb
+++ lib/explorer.rb
@@ -1,13 +1,35 @@
 class Explorer
   attr_accessor :world_grid
 
-  def initialize(world_grid) 
-    raise ArgumentError, "Land Grid is not yet set. should pass 2d array" unless world_grid.is_a?(Array)	
-   
+  def initialize(world_grid)
+    raise ArgumentError, "Land Grid is not yet set. should pass 2d array" unless world_grid.is_a?(Array)
+
     @world_grid = world_grid
   end
-  
-  def explore(state = {:Alive => true}) 
+
+=begin
+  def explore(state = {:Alive => true})
     return 1
+  end
+=end
+
+  def explore x, y
+    current_x = x-1
+    current_y= y-1
+    total_alive = 0
+
+    for i in 0..2
+     for j in 0..2
+       if (world_grid[current_x][current_y] = "*")
+         total_alive +=1
+       end
+       current_y +=1
+     end
+     current_x +=1
+    end
+    if (world_grid[x][y] = "*")
+      total_alive -=1
+    end
+    return total_alive
   end
 end
--- test/test_explorer.rb
+++ test/test_explorer.rb
@@ -2,28 +2,53 @@ require 'test/unit'
 require_relative '../lib/explorer'
 
 class TestSeeder < Test::Unit::TestCase
- 
+
  def setup
    @grid = [[""],[""]]
- end 
- 
+ end
+
  def test_explorer_class_exist
    assert_nothing_raised do
     Explorer.new(@grid)
-   end 
+   end
  end
 
  def test_explorer_initialize_with_world_grid
    assert_nothing_raised "world grid [[][,]] should be initialized" do
     Explorer.new(@grid)
-   end 
+   end
  end
- 
+
+=begin  
  def test_explor_method_accept_hash
    explorer = Explorer.new(@grid)
    assert_nothing_raised "there is an issue with passing state to explore method" do
     state = {:alive => true}
     alive = explorer.explore(state)
-   end 
+   end
  end
-end 
+=end
+
+ def test_explorer_method_should_accept_grid_index
+  grid = Array.new(3){Array.new(3)}
+  explorer = Explorer.new(grid)
+  assert_nothing_raised "explorer method should accept world grid index" do
+    explorer.explore(x=1, y=1)
+  end
+ end
+
+ def test_explorer_count_alive_surrending_cells
+  grid = Array.new(3){Array.new(3)}
+  expected = 8
+  grid[0].size.times do |i|
+    grid[0].size.times do |j|
+      grid[i][j] = "*"
+    end
+  end
+
+  explorer = Explorer.new(grid)
+  actual =  explorer.explore(x=1, y=1)
+  
+  assert_equal expected, actual, "Exploring the grid has problem, should return #{expected} instead of #{actual}"
+ end
+end
