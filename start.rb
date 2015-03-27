e340f41cba19849c14f25034f8d811137c5632c5
Amireza <arfo.amireza@gmail.com>
2015-03-28 01:27:30 +0800

    Adding basic  2d rendering mechanism cml, this method is part of start.rb for now

--- start.rb
+++ start.rb
@@ -1,9 +1,30 @@
 require 'rufus-scheduler'
+require_relative 'lib/tools'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.repeat '1s' do
-  puts 'hi'
+scheduler.repeat '5s' do
+  start_print
 end
+
+def start_print
+ grid = Array.new(40){Array.new(40)}
+ tools = Tools.new()
+ 
+  grid[0].size.times do |i|
+    grid[0].size.times do |j|
+      grid[i][j] = tools.rand_seed 
+    end  
+  end
+
+  
+  grid[0].size.times do |i|
+    grid[0].size.times do |j|
+     print grid[i][j].to_s
+    end 
+    print "\n" 
+  end
+end 
+
 
 scheduler.join
